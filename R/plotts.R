7234642c87a23aa55c4f3a523920854701433ef1
Al Fischer <alphonse.fisch@gmail.com>
2015-02-26 12:25:01 -0500

    added wavcols()
    Added new function: wavcols(); selects colors from a set based on
    wavelengths in sample data

--- R/plotts.R
+++ R/plotts.R
@@ -1,8 +1,9 @@
-plotts <- function(X) {
+plotts <- function(X, wavcols) {
   manipulate(
     {
     tsplot <- ggplot(aes(x = Time, y = `Absorbance (/Mm)`), data = X) +
-      geom_line(aes(color = `Wavelength (nm)`)) + theme_bw()
+      geom_line(aes(color = `Wavelength (nm)`)) + theme_bw() + 
+      scale_color_manual(values = wavcols)
     tsplot + scale_x_datetime(limits = as.POSIXct(c(xmin, xmax), origin = "1970-01-01")) + 
       ylim(ymin, ymax)
   },
---
+++ R/wavcols.R
@@ -0,0 +1,3 @@
+wavcols <- function(x, wavelengths = c(301, 314, 364, 405, 436, 546, 578, 660), colors = c('plum1', 'orchid1', 'darkorchid1', 'purple', 'blue', 'forestgreen', 'goldenrod3', 'red')) {
+  colors[which(wavelengths %in% unique(ts[, 2]))]
+}
