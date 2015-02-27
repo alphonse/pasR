0d638bddb3ebc5a40f9af6589c224a12e7423f86
Al Fischer <alphonse.fisch@gmail.com>
2015-02-27 16:55:32 -0500

    manipulate ymax

--- R/plotts.R
+++ R/plotts.R
@@ -7,13 +7,13 @@ plotts <- function(X, wavcols) {
     tsplot + scale_x_datetime(limits = as.POSIXct(c(xmin, xmax), origin = "1970-01-01")) + 
       ylim(ymin, ymax)
   },
-  ymin=slider(0, max(X$`Absorbance (/Mm)`)*2, 
+  ymin=slider(min(X$`Absorbance (/Mm)`), max(X$`Absorbance (/Mm)`)*1.1, 
               step=signif(max((X$`Absorbance (/Mm)`)-min(X$`Absorbance (/Mm)`))/100, digits=2), 
               initial = min(X$`Absorbance (/Mm)`)),
   xmin=slider(as.numeric(min(X$Time)), as.numeric(max(X$Time)),
               step = 1, 
               initial = as.numeric(min(X$Time))),
-  ymax=slider(0, max(X$`Absorbance (/Mm)`)*2, 
+  ymax=slider(min(X$`Absorbance (/Mm)`), max(X$`Absorbance (/Mm)`)*1.1, 
               step=signif(max((X$`Absorbance (/Mm)`)-min(X$`Absorbance (/Mm)`))/100, digits=2), 
               initial = max(X$`Absorbance (/Mm)`)),
   xmax=slider(as.numeric(min(X$Time)), as.numeric(max(X$Time)), 
